b8a28be90b3a5f58dd5b139430b0067d076f73e7
Dani <dani.diaz91sfc@gmail.com>
2020-11-04 18:13:30 +0000

    ETL class

---
+++ WeatheringApp/WebApplication1/Utils/ETL.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Utils
+{
+    public class ETL
+    {
+
+        static async Task<float> ParseTimeZone(float data)
+        {
+
+            float result = 0;
+
+            result = data / 3600;
+
+            return result;
+
+        }
+
+        static async Task<string> ParseWeather(int id)
+        {
+            string test = "";
+
+            if (id == 1)
+            {
+
+            }
+            else if (id == 2)
+            {
+
+            }
+
+            return test;
+        }
+
+        static async Task<float> ParseMain(int id)
+        {
+            float test = 0;
+
+            if (id == 1)
+            {
+
+            }
+            else if (id == 2)
+            {
+
+            }
+
+            return test;
+        }
+
+        static async Task<float> ParseWind(int id)
+        {
+            float test = 0;
+
+            if (id == 1)
+            {
+
+            }
+            else if (id == 2)
+            {
+
+            }
+
+            return test;
+        }
+
+        static async Task<float> ParseClouds(int id)
+        {
+            float test = 0;
+
+            if (id == 1)
+            {
+
+            }
+            else if (id == 2)
+            {
+
+            }
+
+            return test;
+        }
+
+        static async Task<double> ParseSys(int id)
+        {
+            double test = 0;
+
+            if (id == 1)
+            {
+
+            }
+            else if (id == 2)
+            {
+
+            }
+
+            return test;
+        }
+    }
+
+
+}
